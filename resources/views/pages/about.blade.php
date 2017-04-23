10efc8d2c5b0db3c66144b432f268fcb8e931ced
Piotr Gębura <piotr.gebura@gmail.com>
2017-04-23 19:38:07 +0200

    Lista technologii w O nas

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,5 +3,26 @@
 @section('title', '- O nas')
 
 @section('content')
-	About Page
+
+<div class="row">
+	<div class="col-md-12">
+	    <h1>Zastosowane technologie:</h1>
+
+	    <ul class="list-group list-technologies">
+		  <li class="list-group-item">PHP 5.6</li>
+		  <li class="list-group-item">MySQL</li>
+		  <li class="list-group-item">Laravel 5.4</li>
+		  <li class="list-group-item">Bootstrap</li>
+		  <li class="list-group-item"><a href="https://www.tinymce.com/" target="_blank" class="no-decoration">Tinymce.com</a></li>
+		  <li class="list-group-item"><a href="https://select2.github.io/" target="_blank" class="no-decoration">Select2</a></li>
+		  <li class="list-group-item"><a href="http://parsleyjs.org/" target="_blank" class="no-decoration">Parsley</a></li>
+		  <li class="list-group-item"><a href="http://image.intervention.io//" target="_blank" class="no-decoration">Intervention Image</li>
+		  <li class="list-group-item"><a href="http://htmlpurifier.org/" target="_blank" class="no-decoration">HTML Purifier<a/></li>
+		  <li class="list-group-item"><a href="https://www.google.com/recaptcha/intro/invisible.html" target="_blank" class="no-decoration">reCaptcha</a></li>
+		  <li class="list-group-item"><a href="http://docs.guzzlephp.org" target="_blank" class="no-decoration">Guzzle</a></li>
+		</ul>
+
+	</div>
+</div>
+
 @endsection
--- resources/views/services/specializations/show.blade.php
+++ resources/views/services/specializations/show.blade.php
@@ -16,7 +16,7 @@
 			@foreach ($specialization->doctors as $doctor)
 				<div class="well">
 					<h3>
-						{{ Html::linkRoute('services.doctors.show', $doctor->academic_title.' '.$doctor->first_name.' '.$doctor->last_name, array('id' => $doctor->id), ['class' => 'doctor-link']) }}
+						{{ Html::linkRoute('services.doctors.show', $doctor->academic_title.' '.$doctor->first_name.' '.$doctor->last_name, array('id' => $doctor->id), ['class' => 'doctor-link no-decoration']) }}
 					</h3>
 					<div>
 						@foreach ($doctor->facilities as $facility)
