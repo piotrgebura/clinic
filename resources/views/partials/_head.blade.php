a16dea9665eace27358b3403175c16df87c27087
Piotr Gębura <piotr.gebura@gmail.com>
2017-09-30 23:44:23 +0200

    Poprawka związana z MIX

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -2,8 +2,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Klinika @yield('title')</title>
 
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
 <link href="{{mix('css/app.css')}}" rel="stylesheet" type="text/css" />
 
 @yield('stylesheets')
