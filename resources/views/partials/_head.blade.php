e98b8fd0acffeb606e03d45f95c5d458ff6d1299
Piotr Gębura <piotr.gebura@gmail.com>
2017-09-30 22:37:15 +0200

    MIX dla css i js

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -4,6 +4,6 @@
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-{!! Html::style('css/styles.css') !!}
+<link href="{{mix('css/app.css')}}" rel="stylesheet" type="text/css" />
 
 @yield('stylesheets')
